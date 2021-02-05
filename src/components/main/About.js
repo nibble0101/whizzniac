33d1143fa54c5fae6db328d1bad29e27c8f9351b
nibble0101 <mj.mawa@yahoo.com>
2021-02-05 18:12:31 +0300

    Worked on About page

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 
 function About() {
   return (
-    <section>
-      <p> Hi there, welcome to Whizzniac</p>
+    <section className="about-section">
+      <h1> Hi there, welcome to Whizzniac</h1>
       <p>
-        Would you like to start attempting quiz?
+        This is where education meets recreation. You will learn while having fun. 
+        If you have ever been here before, you can navigate to the history page and
+        resume from where you stopped. Yes, that is how smart we are. We let you take
+        a break in the middle of a quiz or close the app if you like and when you come 
+        come back, you start right from where you stopped. If this is your first time here, 
+        start a new quiz session by clicking the button below. You will be taken to a
+        page where you will select quiz category and difficulty level before you start 
+        attempting the questions. 
       </p>
+      <h2>Enjoy</h2>
       <p>
         <Link to="/categories"> Start Quiz </Link> 
       </p>
