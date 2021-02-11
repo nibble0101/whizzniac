995c157036e68666df77fd497c3b848d52d85854
nibble0101 <mj.mawa@yahoo.com>
2021-02-11 23:12:33 +0300

    Added hamburger menu

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import HamburgerMenu from "react-hamburger-menu";
 
 function Navigation(props) {
+  const [displayMenu, setDisplayMenu] = useState(false);
+  
+  function hamburgerMenuClicHandler() {
+    const body = document.querySelector("body");
+    body.classList.toggle("lock-scroll");
+    setDisplayMenu((displayFlag) => !displayFlag);
+  }
   return (
-    <nav>
-      <ul className="header__nav">
+    <nav className="nav">
+      <HamburgerMenu
+        className="hamburger-menu-icon"
+        width={18}
+        height={15}
+        strokeWidth={1}
+        rotate={0}
+        color="white"
+        borderRadius={0}
+        animationDuration={0.5}
+        isOpen={displayMenu}
+        menuClicked={hamburgerMenuClicHandler}
+      />
+      <ul
+        className={`header__nav ${displayMenu ? "display__hamburger" : ""}`}
+        onClick={hamburgerMenuClicHandler}
+      >
         <li>
-          <NavLink exact to="/">About</NavLink>
+          <NavLink exact to="/">
+            About
+          </NavLink>
         </li>
         <li>
           <NavLink to="/history">History</NavLink>
