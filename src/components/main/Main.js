d3bd72d7f56a132addf02ba26cb8baaf25012182
nibble0101 <mj.mawa@yahoo.com>
2021-02-13 18:38:24 +0300

    Add style and delete unused images

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -8,18 +8,11 @@ import { SocialMedia } from "./SocialMedia";
 import { Solutions } from "./solutions/Solutions";
 import { Error } from "./error/Error";
 import { Switch, Route } from "react-router-dom";
-import {
-  getHeaderHeight,
-  getViewportHeight,
-} from "../../utils/css-styles-utils";
 import "../../styles/Main.css";
 
 function Main() {
   return (
-    <main
-      className="main"
-      style={{ minHeight: `${getViewportHeight() - getHeaderHeight()}px` }}
-    >
+    <main className="main">
       <SocialMedia />
       <Switch>
         <Route exact path="/">
