060609a115f492d045ebbdb37f6539fc6cc2c5d1
nibble0101 <mj.mawa@yahoo.com>
2021-06-06 21:16:24 +0300

    Refactor: Change anchor tag to Link tag in About component

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -8,9 +8,9 @@ function About() {
       <p>
         This is where education meets recreation. You will learn while having
         fun. If you have ever been here before, navigate to the{" "}
-        <a href="/history" className="link">
+        <Link to="/history" className="link">
           history
-        </a>{" "}
+        </Link>{" "}
         page and check out your performance on previous attempts. If this is
         your first time here, start a new quiz session by clicking the button
         below. You will be taken to a page where you will select quiz category
