5154d64ed845901c97aa5902876883ceee7a7188
nibble0101 <mj.mawa@yahoo.com>
2021-07-17 10:52:29 +0300

    test:test utility function for adding or retrieving categories from local storage

--- src/tests/generic-utils.test.js
+++ src/tests/generic-utils.test.js
@@ -17,8 +17,37 @@ describe("Testing utility functions", () => {
     expect(typeof parseQueryString === "function").toBe(true);
     expect(parsedQueryStr).toEqual({ category: "12" });
   });
-  test("Testing setCategoriesToLocalStorage and getCategoriesFromLocalStorage", () => {
+  test("Testing setCategoriesToLocalStorage", () => {
+    setCategoriesToLocalStorage("testing", { a: 1, b: 2 });
     expect(typeof setCategoriesToLocalStorage === "function").toBe(true);
+    expect(JSON.parse(localStorage.getItem("testing"))).toEqual({ a: 1, b: 2 });
+    localStorage.removeItem("testing");
+  });
+  test("Testing getCategoriesFromLocalStorage", () => {
+    const oneDay = 1 * 24 * 60 * 60 * 1000; // ms
+    const now = Date.now();
+    localStorage.setItem(
+      "savedAfterOneWeek",
+      JSON.stringify({
+        dateSaved: now - 8 * oneDay,
+        categories: [{ a: 1 }],
+      })
+    );
+    localStorage.setItem(
+      "savedWithinOneWeek",
+      JSON.stringify({
+        dateSaved: now,
+        categories: [{ a: 1 }],
+      })
+    );
+
     expect(typeof getCategoriesFromLocalStorage === "function").toBe(true);
+    expect(getCategoriesFromLocalStorage("nonExistentKey")).toEqual([]);
+    expect(getCategoriesFromLocalStorage("savedAfterOneWeek")).toEqual([]);
+    expect(getCategoriesFromLocalStorage("savedWithinOneWeek")).toEqual([
+      { a: 1 },
+    ]);
+    localStorage.removeItem("savedAfterOneWeek");
+    localStorage.removeItem("savedWithinOneWeek");
   });
 });
